c1cf847081e17a70301103ea0dd33ddcda035676
Kingxtrem <tantubaykingshuk@gmail.com>
2025-05-14 04:04:46 +0530

    multer file size updated to 5mb

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,17 +1,16 @@
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-dotenv.config()
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
-    let dbUrl = "mongodb+srv://tantubaykingshuk:9MJE7RO9IVQKKkgx@cluster0.2liq5fj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    try {
-        await mongoose.connect(dbUrl);
-        console.log("mongodb connected succefully")
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1)
-    }
-}
+  let dbUrl = process.env.MONGO_URL;
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("mongodb connected succefully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- middleware/multer.js
+++ middleware/multer.js
@@ -12,6 +12,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 2000000 } 
+    limits: { fileSize: 5000000 } 
   });
 module.exports = upload;
