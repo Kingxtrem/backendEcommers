b4679b3d64a173181c24aeccb64ded99504a394b
Kingxtrem <tantubaykingshuk@gmail.com>
2025-06-15 14:54:10 +0530

    vercel.json

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,17 +1,10 @@
 const multer = require('multer');
-const path = require('path');
 
-
-const storage = multer.diskStorage({
-    destination: './uploads/',
-    filename:  (req, file, cb)=> {
-      cb(null,Date.now() + path.extname(file.originalname));
-    }
-  });
-
+const storage = multer.memoryStorage(); // 🟢 IN-MEMORY storage
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 5000000 } 
-  });
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+});
+
 module.exports = upload;
